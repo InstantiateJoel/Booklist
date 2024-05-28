11e9e2fd29ba3b39c2c851b0e026246bc4a65986
DefJoel <joel.scholz.vib@gmail.com>
2024-05-28 21:00:59 +0200

    added the user options and added a todo with ideas how to proceed

--- src/Books.java
+++ src/Books.java
@@ -1,3 +1,49 @@
-public class Books {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
 
+/*
+ Ideas:
+ - When user wants to filter for specific
+ */
+public class Books {
+    // Scanner initialization
+    Scanner userInput = new Scanner(System.in);
+
+    // public var declarations
+    String userOption = ""; // either variable or array, to check if user input is in options
+
+    // public SQL Queries
+    public static void userOptions() {
+        System.out.println("============Booklist============");
+        System.out.println();
+        System.out.println("Please choose from the following options:\n " +
+                "1: Add book\n " +
+                "2: Change Status\n " +
+                "3: Change book\n " +
+                "4: Filter\n " +
+                "5: Delete book");
+        /*
+        todo:
+        - validate the user input
+        - after the user input is one of the following:
+            switch case, to check which one and then go to the part, that executes it
+         */
+    }
+
+    public static void main(String[] args) {
+        try {
+            // connection to database
+            Connection booklistConnection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/booklist", "root", "Tzz$dJG+YccV^HQs");
+
+            // start of the program
+            userOptions();
+            // closing of resources
+            booklistConnection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+    }
 }
